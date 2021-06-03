2a3ff778d05792c76c28d13ec1bdfa299b4b865c
Sourav <panjasourav03@gmail.com>
2021-06-03 13:14:18 +0530

    New 1

--- project/app/models.py
+++ project/app/models.py
@@ -6,13 +6,14 @@ class customerDetails(models.Model):
     address = models.CharField(max_length=100)
     phoneNo = models.CharField(max_length=100)
     email = models.EmailField()
+    emi = models.CharField(max_length=10, null=True, blank=True)
 
     modelNameOne = models.CharField(max_length=100)
     quantityOne = models.CharField(max_length=2, null=True)
     modelNoOne = models.CharField(max_length=100)
-    modelNameTwo = models.CharField(max_length=100)
-    quantityTwo = models.CharField(max_length=2, null=True)
-    modelNoTwo = models.CharField(max_length=100)
+    modelNameTwo = models.CharField(max_length=100, blank=True)
+    quantityTwo = models.CharField(max_length=2, null=True, blank=True)
+    modelNoTwo = models.CharField(max_length=100, blank=True)
     purchaseDate = models.DateField(auto_now=False, auto_now_add=False)
     totalAmount = models.DecimalField(max_digits=10, decimal_places=2)
     advanceAmount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -27,3 +28,7 @@ class paymentOnEmi(models.Model):
     refNo = models.IntegerField()
     paidAmount = models.DecimalField(max_digits=10, decimal_places=2)
     paymentDate = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return str(self.refNo)
+
