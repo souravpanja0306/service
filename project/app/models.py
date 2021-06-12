02b41fd766cfa7f62fee5e04daa1ba5d0f25fc12
Sourav <panjasourav03@gmail.com>
2021-06-12 18:47:01 +0530

    Setup

--- project/app/models.py
+++ project/app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.db.models.aggregates import Aggregate, Sum
+from datetime import datetime, date, timedelta
 
 class customerDetails(models.Model):
     custimerId = models.AutoField(primary_key=True)
@@ -27,19 +28,33 @@ class customerDetails(models.Model):
     def __str__(self):
         return self.name
 
-    def amount_due(self):
+    def amount_due(self): #for due amount 
         payable = self.totalAmount
-        paid = self.advanceAmount
+        paid = self.total_paid_amount()
         return payable - paid
-    
-    def totalAmt(self, id):
-        items = paymentOnEmi.objects.filter(refNo=id)
+
+    def total_paid_amount(self): #for total paid amount
+        tt = self.advanceAmount
+        ap = self.amount_paid()
+        return tt + ap
+
+    def amount_paid(self): #for paymennt
+        items = paymentOnEmi.objects.filter(refNo=self.custimerId)
         total = 0
         for item in items:
             total += item.paidAmount
         return total
 
-        
+    def service_Calculation(self):
+        if self.service == "4":
+            td = timedelta(days = 73)
+            first_ser = self.purchaseDate 
+            return first_ser + td
+        elif self.service == "3":
+            tdd = timedelta(days = 91)
+            first_service = self.purchaseDate
+            return first_service  + tdd
+            
 # Payment Receipt........................................................
 class paymentOnEmi(models.Model):
     refNo = models.IntegerField()
@@ -50,7 +65,6 @@ class paymentOnEmi(models.Model):
 
     def __str__(self):
         return str(self.refNo)
-
 
 # Technician Registrations...............................................
 class techName(models.Model):
